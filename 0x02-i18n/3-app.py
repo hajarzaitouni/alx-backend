c1f85ad62dea6c18c15bf5d4ba5f569575dc57ad
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-07-03 23:02:06 +0100

    fix one error

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -3,6 +3,7 @@
 
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
+from typing import Union
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -19,7 +20,7 @@ app.config.from_object(Config)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> Union[str, None]:
     """ Determine the best match for supported languages """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
