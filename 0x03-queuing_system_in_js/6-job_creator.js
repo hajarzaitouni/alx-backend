0df5d5d0d332d7b78216172314911fdde281735f
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-08-01 01:02:40 +0100

    task 6

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,21 @@
+const kue = require('kue');
+
+const queue = kue.createQueue();
+
+const jobData = {
+  phoneNumber: '2126333333',
+  message: 'Hello, this message is sent using Kue!'
+}
+
+const job = queue.create('push_notification_code', jobData)
+  .save((err) => {
+    if (!err) console.log(`Notification job created: ${job.id}`);
+  });
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
